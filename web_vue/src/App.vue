fa27487c3eca0e654e86687604a28b8a9b72834b
ssp <71054301+BigFaceMan@users.noreply.github.com>
2025-03-15 22:14:29 +0800

    add backgroud image

--- web_vue/src/App.vue
+++ web_vue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :style="backgroundStyle">
     <div v-if="$store.state.themeSet.useTopNav">
       <NavBarRow />
       <router-view></router-view>
@@ -19,6 +19,7 @@ import NavBarRow from "./components/NavBarRow.vue";
 import NavBar from "./components/NavBar.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import { computed } from "vue";
 import { useStore } from "vuex";
 
 export default {
@@ -29,9 +30,23 @@ export default {
   setup() {
     // 这里可以通过某个配置决定默认是顶部导航还是侧边栏
     const store = useStore();
+ // 计算属性，根据导航模式返回不同的背景图片
+    const backgroundStyle = computed(() => {
+      return {
+        backgroundImage: store.state.themeSet.useTopNav
+          ? `url(${new URL('@/assets/images/background.png', import.meta.url)})`
+          : `url(${new URL('@/assets/images/theme2.jpg', import.meta.url)})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh", // 关键：确保背景容器高度填满视口
+        width: "100vw", // 关键：确保背景横向填满
+      };
+    });
 
     return {
-      store
+      store,
+      backgroundStyle
     };
   },
 };
@@ -48,10 +63,10 @@ export default {
   margin-left: 250px;
 }
 
-body {
+/* body {
   background-image: url("@/assets/images/background.png");
   background-size: cover;
-}
+} */
 
 #app {
   background-color: var(--primary-bg);
